3cb8af589b9c73c6d824c3d8fbf1cc16127c17c2
あーる <riwn0122@gmail.com>
2023-08-27 00:23:45 +0900

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'あーるのシレン2日記';
+export const SITE_DESCRIPTION = 'このサイトは風来のシレン2 鬼襲来シレン城のTAに関することを書き連ねるブログです。';
